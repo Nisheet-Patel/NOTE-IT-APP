1b650001a00d422aeaa5d9475300b73df5e87870
Nisheet <62321150+NisheetKumar@users.noreply.github.com>
2022-01-26 13:01:26 +0530

    🌐Modified according to pythonanywhere

--- website/__init__.py
+++ website/__init__.py
@@ -6,8 +6,8 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['database_uri']
-    app.config['SECRET_KEY'] = os.environ['secret_key']
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+    app.config['SECRET_KEY'] = os.getenv('secret_key')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
